0cc34a7e1280fb14b9c2e81ba94c1c9e7f75066c
GloamingGlowGlow <2663058098@qq.com>
2025-04-25 17:32:24 +0800

    BuckBrain_synchronized-new

---
+++ src/main/java/ui/Main.java
@@ -0,0 +1,11 @@
+package ui;
+
+import ui.FinanceTrackerUI;
+import ui.LoginPage;
+
+public class Main {
+    public static void main(String[] args) {
+        // 启动登录页面
+        new LoginPage();
+    }
+}
